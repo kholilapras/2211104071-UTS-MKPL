4703856f8c7cec226343b505fe55bfab07e11d30
kholilapras <kholilapras@gmail.com>
2025-04-19 10:45:52 +0700

    Refactor kode di setMonthlySalary menggunakan base salary extraction and switch

--- src/lib/Employee.java
+++ src/lib/Employee.java
@@ -52,24 +52,21 @@ public class Employee {
 	 * Jika pegawai adalah warga negara asing gaji bulanan diperbesar sebanyak 50%
 	 */
 	
-	public void setMonthlySalary(int grade) {	
-		if (grade == 1) {
-			monthlySalary = 3000000;
-			if (isForeigner) {
-				monthlySalary = (int) (3000000 * 1.5);
-			}
-		}else if (grade == 2) {
-			monthlySalary = 5000000;
-			if (isForeigner) {
-				monthlySalary = (int) (3000000 * 1.5);
-			}
-		}else if (grade == 3) {
-			monthlySalary = 7000000;
-			if (isForeigner) {
-				monthlySalary = (int) (3000000 * 1.5);
-			}
+	 public void setMonthlySalary(int grade) {
+		int baseSalary = 0;
+	
+		switch (grade) {
+			case 1: baseSalary = 3000000; break;
+			case 2: baseSalary = 5000000; break;
+			case 3: baseSalary = 7000000; break;
+			default: 
+				System.err.println("Invalid grade: " + grade);
+				return;
 		}
+	
+		monthlySalary = isForeigner ? (int) (baseSalary * 1.5) : baseSalary;
 	}
+	
 	
 	public void setAnnualDeductible(int deductible) {	
 		this.annualDeductible = deductible;
